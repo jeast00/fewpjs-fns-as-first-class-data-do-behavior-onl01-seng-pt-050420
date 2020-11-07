8382c7bf5f73ee23fa51eeaa839ebf49be42ac6a
Jonathan East <jonathan_east@ymail.com>
2020-11-07 16:16:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  if string < "12:00" {
+    return "Good Morning";
+  else if string >= "12:00" && string <= "17:00" {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
